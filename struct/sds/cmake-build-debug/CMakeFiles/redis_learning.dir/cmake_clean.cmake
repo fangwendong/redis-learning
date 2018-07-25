a5a930d4c4418fc74cbba7bc3edaa9f07ff6e66b
wendong <fang.wendong@myquan.cn>
2018-07-25 10:46:38 +0800

    sds阅读

---
+++ struct/sds/cmake-build-debug/CMakeFiles/redis_learning.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/redis_learning.dir/sds.c.o"
+  "CMakeFiles/redis_learning.dir/home/wendong/CLionProjects/redis-learning/gc/zmalloc.c.o"
+  "redis_learning.pdb"
+  "redis_learning"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/redis_learning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ struct/sds/cmake-build-debug/CMakeFiles/sds.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sds.dir/sds.c.o"
+  "CMakeFiles/sds.dir/home/wendong/CLionProjects/redis-learning/gc/zmalloc.c.o"
+  "sds.pdb"
+  "sds"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sds.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
